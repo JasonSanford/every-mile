5713457919bd0d9ea172dba71c952a0ff6f09cea
Jason Sanford <jasonsanford@gmail.com>
2021-07-23 12:17:46 -0400

    Elevation progress

--- src/elevation_finder.ts
+++ src/elevation_finder.ts
@@ -1,40 +1,45 @@
+import fs from 'fs';
+
 import tilebelt from '@mapbox/tilebelt';
 import getPixels from 'get-pixels';
+import fetch from 'node-fetch';
 
+const getImageFilePath = (z: number, x: number, y: number) => {
+  const fileName = `${z}_${x}_${y}.pngraw`;
+  // `${__dirname}/../../geom/${fileName}`;
+  return `tmp/elevation_images/${fileName}`;
+};
 
-// function getElevation(latitude: number, longitude: number, cb) {
-//   var tileFraction = tilebelt.pointToTileFraction(longitude, latitude, 15);
-//   var tile = tileFraction.map(Math.floor);
-//   var domain = 'https://api.mapbox.com/v4/';
-//   var source = `mapbox.terrain-rgb/${tile[2]}/${tile[0]}/${tile[1]}.pngraw`;
-//   var url = `${domain}${source}?access_token=pk.eyJ1IjoiamNzYW5mb3JkIiwiYSI6ImNrcmYyejJwMzA1ZW0yb29kcGd2aXYzNm8ifQ.Xb6PPg3uG0WCgVffY1xTlg`;
-//   getPixels(url, function(err, pixels) {
-//     if (err) return cb(err);
-//     var xp = tileFraction[0] - tile[0];
-//     var yp = tileFraction[1] - tile[1];
-//     var x = Math.floor(xp*pixels.shape[0]);
-//     var y = Math.floor(yp*pixels.shape[1]);
-
-//     var R = pixels.get(x, y, 0);
-//     var G = pixels.get(x, y, 1);
-//     var B = pixels.get(x, y, 2);
-
-//     var height = -10000 + ((R * 256 * 256 + G * 256 + B) * 0.1);
-
-//     return cb(null, height);
-
-//   });
-// }
+const getImageTile = async (z: number, x: number, y: number) => {
+  const urlFileName = `${z}/${x}/${y}.pngraw`;
+  // const fileName = `${z}_${x}_${y}.pngraw`;
+  // const filePath = `tmp/elevation_images/${fileName}`;
+  const filePath = getImageFilePath(z, x, y);
+  if (fs.existsSync(filePath)) {
+    console.log('Cache hit');
+    const file = fs.readFileSync(filePath);
+    return;
+  }
+  console.log('Cache miss');
+  const url = `https://api.mapbox.com/v4/mapbox.terrain-rgb/${urlFileName}?access_token=pk.eyJ1IjoiamNzYW5mb3JkIiwiYSI6ImNrcmYyejJwMzA1ZW0yb29kcGd2aXYzNm8ifQ.Xb6PPg3uG0WCgVffY1xTlg`;
+  // console.log(url);
+  const response = await fetch(url);
+  const buffer = await response.buffer();
+  console.log(filePath);
+  fs.writeFileSync(filePath, buffer);
+  // return new Uint8Array(buffer);
+  // return new Uint8Array(response.buffer)
+};
 
 class ElevationFinder {
   async getElevation(latitude: number, longitude: number) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       const tileFraction = tilebelt.pointToTileFraction(longitude, latitude, 15);
       const tile = tileFraction.map(Math.floor);
-      const domain = 'https://api.mapbox.com/v4/';
-      const source = `mapbox.terrain-rgb/${tile[2]}/${tile[0]}/${tile[1]}.pngraw`;
-      const url = `${domain}${source}?access_token=pk.eyJ1IjoiamNzYW5mb3JkIiwiYSI6ImNrcmYyejJwMzA1ZW0yb29kcGd2aXYzNm8ifQ.Xb6PPg3uG0WCgVffY1xTlg`;
-      getPixels(url, function(err, pixels) {
+      const [x, y, z] = tile;
+      // const fileName = `${tile[2]}/${tile[0]}/${tile[1]}.pngraw`;
+      await getImageTile(z, x, y);
+      getPixels(getImageFilePath(z, x, y), 'image/png', function(err, pixels) {
         if (err) {
           reject(err);
         }
