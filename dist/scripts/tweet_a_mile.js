34c56e85dfcef25cf24888aad6c7216128a48113
Jason Sanford <jasonsanford@gmail.com>
2021-07-27 09:32:53 -0400

    Start tweeting

--- dist/scripts/tweet_a_mile.js
+++ dist/scripts/tweet_a_mile.js
@@ -4,8 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
+const twitter_lite_1 = __importDefault(require("twitter-lite"));
 const constants_1 = require("../constants");
 const utils_1 = require("./utils");
+const client = new twitter_lite_1.default({
+    // subdomain: "api", // "api" is the default (change for other subdomains)
+    // version: "1.1", // version "1.1" is the default (change for other subdomains)
+    consumer_key: 'U6fkvXwsiznoaFXTXZlwCHCHv',
+    consumer_secret: 'JK4WB2oWB7EI5652Ifi6zxC6EncQILVJ6ZXq7VsUmvDuZKtzJ5',
+    access_token_key: '1418647231897509888-8rzIS3f1M0yCIYgtx2t2H9uNXqhq7i',
+    access_token_secret: 'vGBjSPye2L5ZNmTgwXCuMAWCyWTCHLnIrnRps3vL48NlO' // from your User (oauth_token_secret)
+});
 for (let mile = 1; mile <= constants_1.DISTANCE_MILES; mile++) {
     console.log(`Processing mile ${mile}`);
     const filePath = utils_1.getFilePath(mile);
@@ -15,6 +24,11 @@ for (let mile = 1; mile <= constants_1.DISTANCE_MILES; mile++) {
         console.log(`Tweet mile ${mile}`);
         section.properties.has_tweeted = true;
         fs_1.default.writeFileSync(filePath, JSON.stringify(section));
+        client.post("statuses/update", {
+            status: `Hello world ${mile}`
+        })
+            .then(resp => console.log(resp))
+            .catch(err => console.log(err));
         break;
     }
 }
--- src/scripts/tweet_a_mile.ts
+++ src/scripts/tweet_a_mile.ts
@@ -1,7 +1,17 @@
 import fs from 'fs';
+import Twitter from 'twitter-lite';
 
 import { DISTANCE_MILES } from '../constants';
 import { getFilePath } from './utils';
+
+const client = new Twitter({
+  // subdomain: "api", // "api" is the default (change for other subdomains)
+  // version: "1.1", // version "1.1" is the default (change for other subdomains)
+  consumer_key: 'U6fkvXwsiznoaFXTXZlwCHCHv', // from Twitter.
+  consumer_secret: 'JK4WB2oWB7EI5652Ifi6zxC6EncQILVJ6ZXq7VsUmvDuZKtzJ5', // from Twitter.
+  access_token_key: '1418647231897509888-8rzIS3f1M0yCIYgtx2t2H9uNXqhq7i', // from your User (oauth_token)
+  access_token_secret: 'vGBjSPye2L5ZNmTgwXCuMAWCyWTCHLnIrnRps3vL48NlO' // from your User (oauth_token_secret)
+});
 
 for (let mile = 1; mile <= DISTANCE_MILES; mile++) {
   console.log(`Processing mile ${mile}`);
@@ -14,6 +24,11 @@ for (let mile = 1; mile <= DISTANCE_MILES; mile++) {
     console.log(`Tweet mile ${mile}`);
     section.properties.has_tweeted = true;
     fs.writeFileSync(filePath, JSON.stringify(section));
+      client.post("statuses/update", {
+        status: `Hello world ${mile}`
+      })
+      .then(resp => console.log(resp))
+      .catch(err => console.log(err));
     break;
   }
 }
