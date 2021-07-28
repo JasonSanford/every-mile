7dcedd18dd587096b9f1c348f287af81c48b4a4f
Jason Sanford <jasonsanford@gmail.com>
2021-07-28 07:27:06 -0400

    Reset has_tweeted

---
+++ dist/scripts/reset_has_tweeted.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const constants_1 = require("../constants");
+const utils_1 = require("./utils");
+for (let mile = 1; mile <= constants_1.DISTANCE_MILES; mile++) {
+    console.log(`Processing mile ${mile}`);
+    const filePath = utils_1.getFilePath(mile, 'geojson');
+    const file = fs_1.default.readFileSync(filePath);
+    const section = JSON.parse(file.toString());
+    section.properties.has_tweeted = false;
+    fs_1.default.writeFileSync(filePath, JSON.stringify(section));
+}
---
+++ src/scripts/reset_has_tweeted.ts
@@ -0,0 +1,15 @@
+import fs from 'fs';
+
+import { DISTANCE_MILES } from '../constants';
+import { getFilePath } from './utils';
+
+for (let mile = 1; mile <= DISTANCE_MILES; mile++) {
+  console.log(`Processing mile ${mile}`);
+
+  const filePath = getFilePath(mile, 'geojson');
+  const file = fs.readFileSync(filePath);
+  const section = JSON.parse(file.toString());
+  
+  section.properties.has_tweeted = false;
+  fs.writeFileSync(filePath, JSON.stringify(section));
+}
