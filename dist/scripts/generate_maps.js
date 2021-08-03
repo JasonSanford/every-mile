387998cb3317d48e397deb20bea1c896041c1ef9
Jason Sanford <jasonsanford@gmail.com>
2021-08-03 16:33:50 -0400

    Update some maps

--- dist/scripts/generate_maps.js
+++ dist/scripts/generate_maps.js
@@ -17,7 +17,7 @@ const mapId = 'jcsanford/ckrm3rsr78uiq17q31yhwzul2';
 const dimensions = '700x450';
 const params = { padding, before_layer, access_token };
 const tasks = [];
-for (let mile = 101; mile <= 200; mile++) {
+for (let mile = 201; mile <= 300; mile++) {
     tasks.push((cb) => {
         setTimeout(async () => {
             console.log(`Processing mile ${mile}`);
