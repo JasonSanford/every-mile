c64927e77e4648b77281fc97de1c9e3b607a451e
Jason Sanford <jasonsanford@gmail.com>
2022-02-23 09:39:58 -0500

    Add terrain

--- src/pages/[path_slug]/mile/[mile].tsx
+++ src/pages/[path_slug]/mile/[mile].tsx
@@ -128,6 +128,17 @@ const Mile = ({
           paint: {}
         })
       }
+
+      const currentTerrainSource = map.getSource('terrain');
+      if (!currentTerrainSource) {
+        map.addSource('terrain', {
+          'type': 'raster-dem',
+          'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+          'tileSize': 512,
+          'maxzoom': 14
+        });
+        map.setTerrain({ 'source': 'terrain', 'exaggeration': 1.5 });
+      }
     }
   }, [map, section]);
 
