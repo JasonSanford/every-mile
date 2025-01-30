f98d8a065294d053f106ddf80a3dc214306cb204
Jason Sanford <jasonsanford@gmail.com>
2025-01-30 11:10:57 -0500

    Switch to vercel

--- src/pages/[path_slug]/mile/[mile].tsx
+++ src/pages/[path_slug]/mile/[mile].tsx
@@ -1,43 +1,51 @@
-import fs from 'fs';
-import { useEffect, useState } from 'react';
-import { GetStaticProps, GetStaticPaths } from 'next';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import mapboxgl, { GeoJSONSource, LngLatLike, MapboxGeoJSONFeature } from 'mapbox-gl';
+import fs from "fs";
+import { useEffect, useState } from "react";
+import { GetStaticProps, GetStaticPaths } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import mapboxgl, {
+  GeoJSONSource,
+  LngLatLike,
+  MapboxGeoJSONFeature,
+} from "mapbox-gl";
 
-import { DISTANCES, MAP_IDS } from '../../../constants';
-import { serializePathIdentifierAndMile, pathIdentifierToName, getOgImageUrl, getMilePath, geocodeToLocationString } from '../../../utils';
-import { ParsedUrlQuery } from 'querystring';
-import { getFilePath, metersToFeet } from '../../../scripts/utils';
-import Link from 'next/link';
-import MapboxMap from '../../../components/MapboxMap';
-import { GeocodePart } from '../../../types';
+import { DISTANCES, MAP_IDS } from "../../../constants";
+import {
+  serializePathIdentifierAndMile,
+  pathIdentifierToName,
+  getOgImageUrl,
+  getMilePath,
+  geocodeToLocationString,
+} from "../../../utils";
+import { ParsedUrlQuery } from "querystring";
+import { getFilePath, metersToFeet } from "../../../scripts/utils";
+import Link from "next/link";
+import MapboxMap from "../../../components/MapboxMap";
+import { GeocodePart } from "../../../types";
 
 type LineString = {
   type: string;
   coordinates: LngLatLike[];
-}
+};
 
 type MileProps = {
-  section: MapboxGeoJSONFeature,
+  section: MapboxGeoJSONFeature;
   geocode: GeocodePart[];
   minElevation: number;
   maxElevation: number;
   diffElevation: number;
 };
 
-const Mile = ({
-  section, geocode, maxElevation, diffElevation
-}: MileProps) => {
-  const geometry = (section.geometry as LineString);
+const Mile = ({ section, geocode, maxElevation, diffElevation }: MileProps) => {
+  const geometry = section.geometry as LineString;
   const router = useRouter();
   const serialized = serializePathIdentifierAndMile(router.query);
-  
+
   if (!serialized) {
     // TODO: 404
     return <p>nope</p>;
   }
-  
+
   const { path, mile } = serialized;
   const name = pathIdentifierToName(path);
 
@@ -48,9 +56,11 @@ const Mile = ({
 
     adjacentMiles.push(
       <Link href={getMilePath(path, previousMile)}>
-        <button className="rounded-full">&larr; Previous Mile ({previousMile})</button>
+        <button className="rounded-full">
+          &larr; Previous Mile ({previousMile})
+        </button>
       </Link>
-    )
+    );
   }
 
   const [map, setMap] = useState<mapboxgl.Map | null>(null);
@@ -62,7 +72,7 @@ const Mile = ({
       <Link href={getMilePath(path, nextMile)}>
         <button className="rounded-full">Next Mile ({nextMile}) &rarr;</button>
       </Link>
-    )
+    );
   }
 
   if (adjacentMiles.length === 2) {
@@ -82,77 +92,88 @@ const Mile = ({
 
       map.fitBounds(bounds, { padding: 100 });
 
-      const currentSource = map.getSource('mile');
+      const currentSource = map.getSource("mile");
       if (currentSource) {
         (currentSource as GeoJSONSource).setData(section);
       } else {
-        map.addSource('mile', {
-          type: 'geojson',
-          data: section
+        map.addSource("mile", {
+          type: "geojson",
+          data: section,
         });
       }
 
-      const currentLineLayer = map.getLayer('mile-line-layer');
+      const currentLineLayer = map.getLayer("mile-line-layer");
       if (!currentLineLayer) {
         map.addLayer({
-          'id': 'mile-line-layer',
-          'type': 'line',
-          'source': 'mile',
-          'layout': {
-            'line-cap': 'round',
-            'line-join': 'round',
+          id: "mile-line-layer",
+          type: "line",
+          source: "mile",
+          layout: {
+            "line-cap": "round",
+            "line-join": "round",
           },
-          'paint': {
-            'line-opacity': 1,
-            'line-color': '#04b262',
-            'line-width': 12,
-          }
+          paint: {
+            "line-opacity": 1,
+            "line-color": "#04b262",
+            "line-width": 12,
+          },
         });
       }
 
-      const currentFillLayer = map.getLayer('mile-fill-layer');
+      const currentFillLayer = map.getLayer("mile-fill-layer");
       if (!currentFillLayer) {
         map.addLayer({
-          id: 'mile-fill-layer',
-          type: 'symbol',
-          source: 'mile',
+          id: "mile-fill-layer",
+          type: "symbol",
+          source: "mile",
           layout: {
-            'icon-allow-overlap': true,
-            'icon-ignore-placement': true,
-            'icon-image': ['image', 'direction-marker'],
-            'icon-rotate': 90,
-            'symbol-placement': 'line',
-            'symbol-spacing': 30,
-            'icon-size': 0.8
+            "icon-allow-overlap": true,
+            "icon-ignore-placement": true,
+            "icon-image": ["image", "direction-marker"],
+            "icon-rotate": 90,
+            "symbol-placement": "line",
+            "symbol-spacing": 30,
+            "icon-size": 0.8,
           },
-          paint: {}
-        })
+          paint: {},
+        });
       }
 
-      const currentTerrainSource = map.getSource('terrain');
+      const currentTerrainSource = map.getSource("terrain");
       if (!currentTerrainSource) {
-        map.addSource('terrain', {
-          'type': 'raster-dem',
-          'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
-          'tileSize': 512,
-          'maxzoom': 14
+        map.addSource("terrain", {
+          type: "raster-dem",
+          url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+          tileSize: 512,
+          maxzoom: 14,
         });
-        map.setTerrain({ 'source': 'terrain', 'exaggeration': 1.5 });
+        map.setTerrain({ source: "terrain", exaggeration: 1.5 });
       }
     }
   }, [map, section]);
 
-  const elevGainFeetDisplay = parseInt(metersToFeet(diffElevation).toFixed(), 10).toLocaleString();
-  const elevGainMetersDisplay = parseInt(diffElevation.toFixed()).toLocaleString();
+  const elevGainFeetDisplay = parseInt(
+    metersToFeet(diffElevation).toFixed(),
+    10
+  ).toLocaleString();
+  const elevGainMetersDisplay = parseInt(
+    diffElevation.toFixed()
+  ).toLocaleString();
 
-  const maxElevFeetDisplay = parseInt(metersToFeet(maxElevation).toFixed(0)).toLocaleString();
-  const maxElevMetersDisplay = parseInt(maxElevation.toFixed(0)).toLocaleString();
+  const maxElevFeetDisplay = parseInt(
+    metersToFeet(maxElevation).toFixed(0)
+  ).toLocaleString();
+  const maxElevMetersDisplay = parseInt(
+    maxElevation.toFixed(0)
+  ).toLocaleString();
 
   return (
     <>
       <Head>
-        <title>Every Mile - {name} Mile {mile}</title>
-        <meta property="og:image" content={getOgImageUrl(path, mile)}/>
+        <title>
+          Every Mile - {name} Mile {mile}
+        </title>
+        <meta property="og:image" content={getOgImageUrl(path, mile)} />
       </Head>
       <section className="">
         <div className="px-4 mx-auto text-center max-w-7xl sm:px-6 lg:px-8">
@@ -162,27 +183,41 @@ const Mile = ({
           <h2 className="text-3xl font-extrabold tracking-tight text-green-600 sm:text-4xl md:text-5xl xl:text-6xl">
             Mile {mile}
           </h2>
-          <div className="mt-5">
-            {
-              adjacentMiles
-            }
-          </div>
-          <p className="mt-5 md:w-1/2 mx-auto">Mile {mile} of the {name} is located near <strong>{geocodeToLocationString(geocode)}</strong>. It has an elevation change of <strong>{elevGainFeetDisplay} ft ({elevGainMetersDisplay} m)</strong> with a maximum elevation of <strong>{maxElevFeetDisplay} ft ({maxElevMetersDisplay} m)</strong>.</p>
-          <div className="map-container shadow-xl rounded-lg mt-5 mb-10" style={{height: 'calc(100vh - 200px)'}}>
+          <div className="mt-5">{adjacentMiles}</div>
+          <p className="mt-5 md:w-1/2 mx-auto">
+            Mile {mile} of the {name} is located near{" "}
+            <strong>{geocodeToLocationString(geocode)}</strong>. It has an
+            elevation change of{" "}
+            <strong>
+              {elevGainFeetDisplay} ft ({elevGainMetersDisplay} m)
+            </strong>{" "}
+            with a maximum elevation of{" "}
+            <strong>
+              {maxElevFeetDisplay} ft ({maxElevMetersDisplay} m)
+            </strong>
+            .
+          </p>
+          <div
+            className="map-container shadow-xl rounded-lg mt-5 mb-10"
+            style={{ height: "calc(100vh - 200px)" }}
+          >
             <MapboxMap
               initialOptions={{
                 center: geometry.coordinates[0],
                 zoom: 14,
-                style: `mapbox://styles/${MAP_IDS[path]}`
+                style: `mapbox://styles/${MAP_IDS[path]}`,
               }}
               onLoaded={(map) => {
                 setMap(map);
-                map.loadImage('https://www.everymile.xyz/images/chevron.png', (error, image) => {
-                  if (error || !image) {
-                    return;
-                  };
-                  map.addImage('direction-marker', image);
-                });
+                map.loadImage(
+                  "https://every-mile.vercel.app/images/chevron.png",
+                  (error, image) => {
+                    if (error || !image) {
+                      return;
+                    }
+                    map.addImage("direction-marker", image);
+                  }
+                );
               }}
             />
           </div>
@@ -196,15 +231,29 @@ export const getStaticPaths: GetStaticPaths = async () => {
   return new Promise((resolve) => {
     const paths = [];
 
-    const atPaths = Array(DISTANCES.at).fill(1).map((_, index) => ( { params: { path_slug: 'appalachian-trail', mile: (index + 1).toString() }}));
-    const brpPaths = Array(DISTANCES.brp).fill(1).map((_, index) => ( { params: { path_slug: 'blue-ridge-parkway', mile: (index + 1).toString() }}));
+    const atPaths = Array(DISTANCES.at)
+      .fill(1)
+      .map((_, index) => ({
+        params: {
+          path_slug: "appalachian-trail",
+          mile: (index + 1).toString(),
+        },
+      }));
+    const brpPaths = Array(DISTANCES.brp)
+      .fill(1)
+      .map((_, index) => ({
+        params: {
+          path_slug: "blue-ridge-parkway",
+          mile: (index + 1).toString(),
+        },
+      }));
 
     paths.push(...atPaths, ...brpPaths);
 
     resolve({
       paths,
-      fallback: false
-    })
+      fallback: false,
+    });
   });
 };
 
@@ -212,7 +261,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const serialized = serializePathIdentifierAndMile(params as ParsedUrlQuery);
 
   if (serialized) {
-    const path = getFilePath(serialized.path, serialized.mile, 'geojson');
+    const path = getFilePath(serialized.path, serialized.mile, "geojson");
     const file = fs.readFileSync(path);
     const section = JSON.parse(file.toString());
     const { properties } = section;
@@ -223,12 +272,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         geocode: properties.geocode,
         minElevation: properties.min_elevation,
         maxElevation: properties.max_elevation,
-        diffElevation: properties.elevation_difference
-      }
+        diffElevation: properties.elevation_difference,
+      },
     };
   }
 
-  return { props: {} }
-}
+  return { props: {} };
+};
 
 export default Mile;
--- src/utils.ts
+++ src/utils.ts
@@ -1,36 +1,43 @@
-import { ParsedUrlQuery } from 'querystring';
+import { ParsedUrlQuery } from "querystring";
 
-import { getDistance } from './scripts/utils';
-import { PathSlug, PathIdentifier, PathIdetifierAndMile, GeocodePart } from './types';
-import { DISTANCES, APPALACHIAN_TRAIL, BLUE_RIDGE_PARKWAY } from './constants';
+import { getDistance } from "./scripts/utils";
+import {
+  PathSlug,
+  PathIdentifier,
+  PathIdetifierAndMile,
+  GeocodePart,
+} from "./types";
+import { DISTANCES, APPALACHIAN_TRAIL, BLUE_RIDGE_PARKWAY } from "./constants";
 
 export const pathSlugToIdentifier = (pathSlug: PathSlug) => {
   return {
-    'appalachian-trail': PathIdentifier.AppalachianTrail,
-    'blue-ridge-parkway': PathIdentifier.BlueRidgeParkway,
+    "appalachian-trail": PathIdentifier.AppalachianTrail,
+    "blue-ridge-parkway": PathIdentifier.BlueRidgeParkway,
   }[pathSlug];
 };
 
 export const pathIdentifierToName = (pathIdentifier: PathIdentifier) => {
   return {
-    'at': APPALACHIAN_TRAIL,
-    'brp': BLUE_RIDGE_PARKWAY,
+    at: APPALACHIAN_TRAIL,
+    brp: BLUE_RIDGE_PARKWAY,
   }[pathIdentifier];
 };
 
 export const pathIdentifierToSlug = (pathIdentifier: PathIdentifier) => {
   return {
-    'at': 'appalachian-trail',
-    'brp': 'blue-ridge-parkway',
+    at: "appalachian-trail",
+    brp: "blue-ridge-parkway",
   }[pathIdentifier];
 };
 
-export const serializePathIdentifierAndMile = (query: ParsedUrlQuery): PathIdetifierAndMile | null => {
+export const serializePathIdentifierAndMile = (
+  query: ParsedUrlQuery
+): PathIdetifierAndMile | null => {
   let { mile: mileString, path_slug: pathSlug } = query;
 
   if (pathSlug) {
     if (Array.isArray(pathSlug)) {
-      pathSlug = pathSlug.join('');
+      pathSlug = pathSlug.join("");
     }
   } else {
     return null;
@@ -38,7 +45,7 @@ export const serializePathIdentifierAndMile = (query: ParsedUrlQuery): PathIdeti
 
   if (mileString) {
     if (Array.isArray(mileString)) {
-      mileString = mileString.join('');
+      mileString = mileString.join("");
     }
   } else {
     return null;
@@ -57,19 +64,21 @@ export const serializePathIdentifierAndMile = (query: ParsedUrlQuery): PathIdeti
 
     return {
       path,
-      mile
+      mile,
     };
   } catch (e) {
     return null;
   }
-}
+};
 
-export const serializePathIdentifier = (query: ParsedUrlQuery): PathIdentifier | null => {
+export const serializePathIdentifier = (
+  query: ParsedUrlQuery
+): PathIdentifier | null => {
   let { path_slug: pathSlug } = query;
 
   if (pathSlug) {
     if (Array.isArray(pathSlug)) {
-      pathSlug = pathSlug.join('');
+      pathSlug = pathSlug.join("");
     }
   } else {
     return null;
@@ -84,14 +93,15 @@ export const serializePathIdentifier = (query: ParsedUrlQuery): PathIdentifier |
   } catch (e) {
     return null;
   }
-}
+};
 
 export const getOgImageUrl = (pathIdentifier: PathIdentifier, mile: number) => {
-  const rootUrl = process.env.NODE_ENV === 'production'
-    ? 'https://everymile.xyz'
-    : 'http://localhost:3000';
+  const rootUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://every-mile.vercel.app"
+      : "http://localhost:3000";
   const padAmount = getDistance(pathIdentifier).toString().length;
-  const fileName = `mile_${mile.toString().padStart(padAmount, '0')}.png`;
+  const fileName = `mile_${mile.toString().padStart(padAmount, "0")}.png`;
   return `${rootUrl}/images/${pathIdentifier}/${fileName}`;
 };
 
@@ -101,9 +111,10 @@ export const getMilePath = (pathIdentifier: PathIdentifier, mile: number) => {
 };
 
 export const getMileUrl = (pathIdentifier: PathIdentifier, mile: number) => {
-  const root = process.env.GITHUB_ACTIONS === 'true'
-    ? 'https://everymile.xyz'
-    : 'http://localhost:3000';
+  const root =
+    process.env.GITHUB_ACTIONS === "true"
+      ? "https://every-mile.vercel.app"
+      : "http://localhost:3000";
   const path = getMilePath(pathIdentifier, mile);
   return `${root}${path}`;
 };
@@ -112,26 +123,28 @@ export const chunkify = (things: Array<any>, chunkSize: number) => {
   const res = [];
 
   for (let i = 0; i < things.length; i += chunkSize) {
-      const chunk = things.slice(i, i + chunkSize);
-      res.push(chunk);
+    const chunk = things.slice(i, i + chunkSize);
+    res.push(chunk);
   }
 
   return res;
-}
+};
 
-export const geocodeToLocationString = (geocode: GeocodePart[]): string | null => {
+export const geocodeToLocationString = (
+  geocode: GeocodePart[]
+): string | null => {
   const locationParts = [];
 
   for (let i = 0; i < geocode.length; i++) {
     const geocodePart = geocode[i];
 
-    if (geocodePart.id.includes('place') || geocodePart.id.includes('region')) {
+    if (geocodePart.id.includes("place") || geocodePart.id.includes("region")) {
       locationParts.push(geocodePart.text);
     }
   }
 
   if (locationParts.length > 0) {
-    return locationParts.join(', ');
+    return locationParts.join(", ");
   }
 
   return null;
